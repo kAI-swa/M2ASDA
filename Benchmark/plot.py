a94da3d3eae851a3a94bd2526b88cd843e75306a
rf2bNdb <134395785+rf2bNdb@users.noreply.github.com>
2024-01-07 19:46:27 +0800

    Update plot.py

--- Benchmark/plot.py
+++ Benchmark/plot.py
@@ -123,3 +123,50 @@ def plot_metric_PRcurve(adata: ad.AnnData):
     for i in range(7):
         plt.setp(leg_lines[i], linewidth=3)
     plt.savefig('metrics.png', dpi=600, bbox_inches='tight')
+
+def plot_MarkerGenes(adata, top_n_genes=3):
+    '''运行时要注意几个问题：
+    1.是否要调整纵轴的精度
+    2.画点图时可以选择输入列表还是字典：如果输入字典的话，最终图的上方就会有好几个方括号分别代表各自的键（细胞），
+      但是这样的话就可能使不同细胞的marker重复；所以为避免这种情况可以选择输入列表（在下方通过更改注释条件即可）
+    '''
+    df = pd.DataFrame(adata.obs['score'])
+    df['Pred_detail'] = 'Potential anomalous'
+    for i in range(len(df)):
+        if df['score'][i]<0.15:
+            df['Pred_detail'][i]='Normal'
+        elif df['score'][i]>0.25:
+            df['Pred_detail'][i]='Anomalous'
+    # df['Pred_detail'] = '0.85-1'
+    # for i in range(len(df)):
+    #     if df['score'][i]<0.15:
+    #         df['Pred_detail'][i]='0-0.15'
+    #     elif df['score'][i]>=0.15 or df['score'][i]<0.25:
+    #         df['Pred_detail'][i]='0.25-0.4'
+    #     elif df['score'][i]>=0.25 or df['score'][i]<0.4:
+    #         df['Pred_detail'][i]='0.25-0.4'
+    #     elif df['score'][i]>=0.4 or df['score'][i]<0.55:
+    #         df['Pred_detail'][i]='0.40-0.55'
+    #     elif df['score'][i]>=0.55 or df['score'][i]<0.7:
+    #         df['Pred_detail'][i]='0.55-0.70'
+    #     elif df['score'][i]>=0.7 or df['score'][i]<0.85:
+    #         df['Pred_detail'][i]='0.70-0.85'
+    adata.obs['Pred_detail']=df['Pred_detail']
+    
+    ## 1.find marker genes
+    sc.tl.rank_genes_groups(adata,groupby='Pred_detail',use_raw=False,method="wilcoxon") #method='t-test','wilcoxon'
+    # sc.pl.rank_genes_groups_matrixplot(adata,groupby='Pred_detail',values_to_plot='logfoldchanges',cmap='bwr')
+    
+    ## 2.select top_n marker genes
+    sc.tl.filter_rank_genes_groups(adata,groupby="Pred_detail",min_in_group_fraction=0.8,max_out_group_fraction=0.2) 
+    # sc.pl.rank_genes_groups_dotplot(adata, groupby="Pred_detail", standard_scale="var", n_genes=top_n_genes,cmap='bwr')
+    markers=sc.get.rank_genes_groups_df(adata, group=adata.obs['Pred_detail'].unique())
+    markers=markers.loc[~ markers.names.isna()]
+    markers['abs_score']=markers.scores.abs()
+    markers.sort_values('abs_score',ascending=False,inplace=True)
+    markers = markers.groupby('group').head(top_n_genes).sort_values('group')['names'].unique()#用list来画图
+    # markers = {group: names.tolist() for group, names in markers.groupby('group')['names']}#用dict来画图（记得注释上一行的最后两个）
+
+    ## 3.use marker genes as dict to group them                      
+    sc.pl.dotplot(adata, markers, groupby = 'Pred_detail', show=False)
+    # plt.savefig('markergenes.png', dpi=600, bbox_inches='tight')
